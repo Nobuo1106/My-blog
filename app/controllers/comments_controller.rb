e6bbceb3456741758724177e026fe54d8670273f
Iga <gonnbe1106@yahoo.co.jp>
2019-02-15 18:35:29 +0900

    added destroy comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,15 @@
 class CommentsController < ApplicationController
+
   def create
     @article = Article.find_by(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find_by(params[:article_id])
+    @comment = @article.comments.find_by(params[:id])
+    @comment.destroy
     redirect_to article_path(@article)
   end
 private
