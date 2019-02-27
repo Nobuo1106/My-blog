a25f2746096a0fce047b64665acead6c02ed0166
Iga <gonnbe1106@yahoo.co.jp>
2019-02-27 15:05:03 +0900

    debugged seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create!(
+   userid: 'admin',
+   email: 'admin@testing.com',
+   password: 'adminpw',
+   password_confirmation: 'adminpw',
+   admin: true
+)
